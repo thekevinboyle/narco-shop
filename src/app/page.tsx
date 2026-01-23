c9d372fcde73f827aac54bff3b0c14e1919ec480
thekevinboyle <thekevinboyle@gmail.com>
2026-01-22 19:22:48 -0600

    Add Header and Footer to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,19 @@ import IntroText from "@/components/home/IntroText";
 import CategoryTiles from "@/components/home/CategoryTiles";
 import FeaturedProduct from "@/components/home/FeaturedProduct";
 import CTASection from "@/components/home/CTASection";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
-    <>
+    <main className="min-h-screen bg-white">
+      <Header />
       <Hero />
       <IntroText />
       <CategoryTiles />
       <FeaturedProduct />
       <CTASection />
-    </>
+      <Footer />
+    </main>
   );
 }
